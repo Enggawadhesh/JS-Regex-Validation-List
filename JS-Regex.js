5fd463f6020a309f3fa0f3b6a6f0ee1735277d87
Awadhesh Kumar <>
2018-04-10 15:55:38 +0530

    added some

--- JS-Regex.js
+++ JS-Regex.js
@@ -6,8 +6,7 @@ let numbers = /^[0-9]*$/;
 let numbers = /^\d*$/;
 
 // 10 digits mobile number (only numbers)
-let mobile = /^[0-9]{10}$/;
-// or
+// You can change length
 let mobile = /^\d{10}$/;
 
 // Alphabets only small letters
@@ -21,5 +20,14 @@ let Alphabet = /^[a-zA-Z]*$/;
 
 // Alphanumeric only with capital and small letters
 let Alphabet = /^[\da-zA-Z]*$/;
-// or
-let Alphabet = /^[0-9a-zA-Z]*$/;
+
+// Username alphanumeric with '_' and 4-8 chars
+// You can edit username length by changing number 4(min) & 8(max)
+let username = /^([\da-zA-Z_]){4,8}$/;
+
+// Email 
+let email = /^\w+([\.-]?\w+)@\w+([-]?\w+)\.([a-z]{2,3})(\.[a-z]{2,3})?$/;
+
+// Web URL
+// Ex. example.com or example.co.in, www.example.com, http://example.com, http://www.example.com
+let url = /^(http[s]?:\/\/)?(www\.)?[a-zA-Z0-9]+([-]?\w+)\.([a-z]{2,3})(\.[a-z]{2,3})?$/;
