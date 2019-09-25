4de7523c22c1ff63192ad47576bc33d90811d6ea
Awadhesh Kumar <awadheshkr876@outlook.com>
2019-09-25 12:57:05 +0530

    check link in text and replace as actual link

--- use-js-regex-validation.js
+++ use-js-regex-validation.js
@@ -53,3 +53,21 @@ inputs.forEach((input) => {
         // console.log(patterns[e.target.attributes.name.value]);
     });
 });
+
+/**
+ *
+ * Find link from text and Replace actual link
+ *
+*/
+urlify(data) {
+    if (data) {
+      var urlRegex = /(((https?:\/\/)|(www\.))[^\s]+)|([^\s]+\.([a-z]{2,})+)|([\.A-z0-9_\-\+]+[@][A-z0-9_\-]+([.][A-z0-9_\-\.]+)+[A-z]{2,4}$)|(\b\d+?\b(?!(?:[\.,]\d+)|(?:\s*(?:%|percent))))/g;
+      return data.replace(urlRegex, function(url) {
+          if(url.match('http://') || url.match('https://')) {
+            return '<a class="link" target="_blank" href="' + url + '">' + url + '</a>';
+          } else {
+            return '<a class="link" target="_blank" href="http://' + url + '">' + url + '</a>';
+          }
+      });
+    }
+  }
